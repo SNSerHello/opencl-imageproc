0852d99f34e62a6997749fe8abf46275d66698c5
SNSerHello <hello.sns@hotmail.com>
2023-03-27 14:36:01 +0800

    [03/27/2023] format source codes of python
    format source codes of python

--- resize.py
+++ resize.py
@@ -2,6 +2,7 @@ import numpy as np
 import pyopencl as cl
 import h5py as h5
 
+
 class CResize(object):
     def __init__(self):
         platforms = cl.get_platforms()
@@ -9,23 +10,39 @@ class CResize(object):
         for platform in platforms:
             devices = platform.get_devices()
             for device in devices:
-                print("  * {0} on {1} ({2})".format(device.name, device.platform.name, device.platform.version))
+                print(
+                    "  * {0} on {1} ({2})".format(
+                        device.name, device.platform.name, device.platform.version
+                    )
+                )
 
         device = platforms[0].get_devices()[0]
-        print("Picked {0} on {1} ({2})".format(device.name, device.platform.name, device.platform.version))
+        print(
+            "Picked {0} on {1} ({2})".format(
+                device.name, device.platform.name, device.platform.version
+            )
+        )
 
         self.ctx = cl.Context([device])
         self.queue = cl.CommandQueue(self.ctx)
 
-        print("Max allowed 2D image size: ({0}, {1})".format( \
-                                                        device.get_info(cl.device_info.IMAGE2D_MAX_WIDTH), \
-                                                        device.get_info(cl.device_info.IMAGE2D_MAX_HEIGHT)))
-        print("Max allowed 3D image size: ({0}, {1}, {2})".format( \
-                                                        device.get_info(cl.device_info.IMAGE3D_MAX_WIDTH), \
-                                                        device.get_info(cl.device_info.IMAGE3D_MAX_HEIGHT), \
-                                                        device.get_info(cl.device_info.IMAGE3D_MAX_DEPTH)))
+        print(
+            "Max allowed 2D image size: ({0}, {1})".format(
+                device.get_info(cl.device_info.IMAGE2D_MAX_WIDTH),
+                device.get_info(cl.device_info.IMAGE2D_MAX_HEIGHT),
+            )
+        )
+        print(
+            "Max allowed 3D image size: ({0}, {1}, {2})".format(
+                device.get_info(cl.device_info.IMAGE3D_MAX_WIDTH),
+                device.get_info(cl.device_info.IMAGE3D_MAX_HEIGHT),
+                device.get_info(cl.device_info.IMAGE3D_MAX_DEPTH),
+            )
+        )
 
-        self.prg_resize = cl.Program(self.ctx, """
+        self.prg_resize = cl.Program(
+            self.ctx,
+            """
             #pragma OPENCL EXTENSION cl_khr_3d_image_writes : enable
             
 
@@ -129,25 +146,37 @@ class CResize(object):
             }
 
 
-            """).build()
+            """,
+        ).build()
 
         self.channel_mapping = {
             1: cl.channel_order.R,
             2: cl.channel_order.RG,
             3: cl.channel_order.RGB,
-            4: cl.channel_order.RGBA
+            4: cl.channel_order.RGBA,
         }
 
     def get_num_channels_and_order(self, src_data, physical_dim):
         num_channels = 1
         if src_data.ndim < physical_dim:
-            raise ValueError(str(physical_dim) + "D image must be of dimension " + str(physical_dim) + \
-                             " if only one channel, or " + str(physical_dim + 1) + ", but was " + str(src_data.ndim))
+            raise ValueError(
+                str(physical_dim)
+                + "D image must be of dimension "
+                + str(physical_dim)
+                + " if only one channel, or "
+                + str(physical_dim + 1)
+                + ", but was "
+                + str(src_data.ndim)
+            )
         elif src_data.ndim == physical_dim + 1:
             num_channels = src_data.shape[physical_dim]
             if num_channels > 4:
-                raise ValueError("Invalid number of channels (" + str(num_channels) + "), must be 1, 2, 3 or 4")
-        
+                raise ValueError(
+                    "Invalid number of channels ("
+                    + str(num_channels)
+                    + "), must be 1, 2, 3 or 4"
+                )
+
         return num_channels, self.channel_mapping[num_channels]
 
     def get_channel_type(self, src_data):
@@ -158,12 +187,18 @@ class CResize(object):
         elif src_data.dtype == np.uint32:
             channel_type = cl.channel_type.UNSIGNED_INT32
         else:
-            raise TypeError(str("Data type " + str(src_data.dtype) + " currently not supported"))
+            raise TypeError(
+                str("Data type " + str(src_data.dtype) + " currently not supported")
+            )
         return channel_type
 
     def resize2D(self, src_data, target_shape, mode="NEAREST"):
         if len(target_shape) != 2:
-            raise ValueError("Invalid target shape dimension (" + str(len(target_shape)) + "). Should be 2-dimensional")
+            raise ValueError(
+                "Invalid target shape dimension ("
+                + str(len(target_shape))
+                + "). Should be 2-dimensional"
+            )
         if any(map(lambda x: x < 1, target_shape)):
             raise ValueError("Invalid target shape: " + str(target_shape))
 
@@ -172,7 +207,9 @@ class CResize(object):
 
         fmt = cl.ImageFormat(channel_order, channel_type)
         src_img = cl.image_from_array(self.ctx, src_data, num_channels=num_channels)
-        dst_img = cl.Image(self.ctx, cl.mem_flags.WRITE_ONLY, fmt, target_shape[::-1]) #img dims are swapped
+        dst_img = cl.Image(
+            self.ctx, cl.mem_flags.WRITE_ONLY, fmt, target_shape[::-1]
+        )  # img dims are swapped
 
         roi_shape = tuple(np.maximum(src_img.shape, dst_img.shape))
 
@@ -188,7 +225,11 @@ class CResize(object):
 
     def resize3D(self, src_data, target_shape, mode="NEAREST"):
         if len(target_shape) != 3:
-            raise ValueError("Invalid target shape dimension (" + str(len(target_shape)) + "). Should be 3-dimensional")
+            raise ValueError(
+                "Invalid target shape dimension ("
+                + str(len(target_shape))
+                + "). Should be 3-dimensional"
+            )
         if any(map(lambda x: x < 1, target_shape)):
             raise ValueError("Invalid target shape: " + str(target_shape))
 
@@ -197,20 +238,23 @@ class CResize(object):
 
         fmt = cl.ImageFormat(channel_order, channel_type)
         src_img = cl.image_from_array(self.ctx, src_data, num_channels=num_channels)
-        dst_img = cl.Image(self.ctx, cl.mem_flags.WRITE_ONLY, fmt, target_shape[::-1]) #img dims are swapped
+        dst_img = cl.Image(
+            self.ctx, cl.mem_flags.WRITE_ONLY, fmt, target_shape[::-1]
+        )  # img dims are swapped
 
         roi_shape = tuple(np.maximum(src_img.shape, dst_img.shape))
-        
+
         if mode == "NEAREST":
             self.prg_resize.nearest3D(self.queue, roi_shape, None, src_img, dst_img)
         elif mode == "LINEAR":
             self.prg_resize.linear3D(self.queue, roi_shape, None, src_img, dst_img)
 
         res = np.empty(target_shape, src_data.dtype)
-        cl.enqueue_copy(self.queue, res, dst_img, origin=(0, 0, 0), region=dst_img.shape)
+        cl.enqueue_copy(
+            self.queue, res, dst_img, origin=(0, 0, 0), region=dst_img.shape
+        )
 
         return res
-    
 
 
 def main():
@@ -218,18 +262,18 @@ def main():
     resizer = CResize()
 
     # Size limitation for now...
-    src_data = np.array(h5.File("4,4_aligned.h5", "r")['img'])[0:16384, 0:16384].copy()
+    src_data = np.array(h5.File("4,4_aligned.h5", "r")["img"])[0:16384, 0:16384].copy()
 
     # Resize!
     res_nearest = resizer.resize2D(src_data, (2048, 2048), mode="NEAREST")
-    res_linear  = resizer.resize2D(src_data, (2048, 2048), mode="LINEAR")
-
+    res_linear = resizer.resize2D(src_data, (2048, 2048), mode="LINEAR")
 
     # Save result
-    testfile = h5.File('output.h5', 'w')
-    testfile.create_dataset('nearest', data=res_nearest)
-    testfile.create_dataset('linear', data=res_linear)
+    testfile = h5.File("output.h5", "w")
+    testfile.create_dataset("nearest", data=res_nearest)
+    testfile.create_dataset("linear", data=res_linear)
     testfile.close()
+
 
 if __name__ == "__main__":
     main()
